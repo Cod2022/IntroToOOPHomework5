c25a0925ebbb4f9ae056e0d636a1962ff9a2cd43
My Name <Me@gmail.com>
2023-01-23 20:39:35 +0400

    2nd commit

--- MyCalc/Main.java
+++ MyCalc/Main.java
@@ -2,7 +2,7 @@ package MyCalc;
 
 public class Main {
     public static void main(String[] args) {
-        String string = "6/4 - 8/9";
+        String string = "6/4 + 8/9";
         RationalCalc calc = new RationalCalc();
         System.out.println(calc.rationalNumSum(string));
     }
